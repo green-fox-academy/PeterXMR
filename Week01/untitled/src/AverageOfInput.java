64793578bef802c578981b5dfb0c239e8285ff07
PeterXMR <peter.kozuch@zoho.com>
2018-09-21 11:39:45 +0200

    another commit

--- Week01/untitled/src/AverageOfInput.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-
-        // Write a program that asks for 5 integers in a row,
-// then it should print the sum and the average of these numbers like:
-//
-// Sum: 22, Average: 4.4
--- Week01/untitled/src/IntroduceYourself.java
+++
@@ -1,7 +0,0 @@
-public class IntroduceYourself {
-    public static void main(String[] args) {
-        System.out.println("Peter Kozuch");
-        System.out.println("24 years");
-        System.out.println("1.79 cm");
-    }
-}
