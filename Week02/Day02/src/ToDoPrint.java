c98eb89f22268fc69a9e4241b1c7d2e77df4becd
PeterXMR <peter.kozuch@zoho.com>
2018-09-25 17:58:30 +0200

    Another Commit

---
+++ Week02/Day02/src/ToDoPrint.java
@@ -0,0 +1,18 @@
+public class ToDoPrint {
+     public static void main(String... args){
+    String todoText = " - Buy milk\n";
+     todoText = todoText + " - Download games\n\r - Diablo";
+         // Add "My todo:" to the beginning of the todoText
+         // Add " - Download games" to the end of the todoText
+         // Add " - Diablo" to the end of the todoText but with indention
+
+         // Expected outpt:
+
+         // My todo:
+         //  - Buy milk
+         //  - Download games
+         //      - Diablo
+
+         System.out.println(todoText);;
+    }
+}
