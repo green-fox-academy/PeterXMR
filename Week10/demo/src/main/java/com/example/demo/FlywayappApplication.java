0ae99963caef47f16282605bf832369b0f27a280
PeterXMR <peter.kozuch@zoho.com>
2018-12-11 13:45:57 +0100

    adding demo app with implemented flyway database migrating

---
+++ Week10/demo/src/main/java/com/example/demo/FlywayappApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayappApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayappApplication.class, args);
+	}
+}
